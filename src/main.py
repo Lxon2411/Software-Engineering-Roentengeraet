03c145fda9a2eb1895f263560b0d0c2dbc4568d0
Lxon2411 <leonwuehr@gmx.de>
2025-12-11 16:30:43 +0100

    Make project executable in terminal

--- src/main.py
+++ src/main.py
@@ -7,16 +7,31 @@ from src.gui.ui import RadiationUI
 from src.controller.controller import RadiationController
 
 
+def resource_path(relative_path: str) -> str:
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(base_path, relative_path)
+
+
 def main():
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    if PROJECT_ROOT not in sys.path:
+        sys.path.insert(0, PROJECT_ROOT)
+
     root = tk.Tk()
     root.title("Röntgengerät Simulation")
     root.geometry("500x550")
     root.resizable(False, False)
 
-    # BASE_DIR = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    # icon_path = os.path.join(BASE_DIR, "icon.png")
-    # icon = PhotoImage(file=icon_path)
-    # root.iconphoto(False, icon)
+    icon_path = resource_path("icon.png")
+    try:
+        icon = PhotoImage(file=icon_path)
+        root.iconphoto(False, icon)
+    except Exception as e:
+        print(f"Icon konnte nicht geladen werden: {e}")
 
     ui = RadiationUI(root, None)
     controller = RadiationController(ui)
